f89884393c69cd6a153f867dea0e2c7d5c470ead
Caiorossi00 <caio_rossi56@hotmail.com>
2025-06-24 08:35:42 -0300

    feat: implement Footer component with styling and links; update Navbar links and adjust Hero styles

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,57 @@
 import React from "react";
+import "../assets/styles/Footer.scss";
 
-const Footer = () => {
-  return <div>Footer</div>;
-};
-
-export default Footer;
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <div className="footer-top">
+        <div className="footer-left">
+          <h1>Left</h1>
+          <div>
+            <a href="#">Início</a>
+            <a href="#sobre">Sobre</a>
+            <a href="#sobre">Ranking</a>
+            <a href="#sobre">Entre em um time</a>
+            <a href="#sobre">Projetos finalizados</a>
+          </div>
+        </div>
+        <div className="footer-right">
+          <h1>Socials</h1>
+          <div>
+            <a
+              href="https://www.instagram.com/caiorossi.dev"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Instagram
+            </a>
+            <a
+              href="https://www.linkedin.com/in/caio-rossi-dev/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              LinkedIn
+            </a>
+            <a
+              href="https://www.linkedin.com/in/caio-rossi-dev/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Github
+            </a>
+            <a
+              href="https://www.linkedin.com/in/caio-rossi-dev/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Youtube
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className="footer-bottom">
+        <p>Developed by Caio Rossi</p>
+      </div>
+    </footer>
+  );
+}
